7cce72adc5476f1eb34f15f3b4f89ea19f8a7385
Gaël Le Bon <gael.m.lebon@gmail.com>
2016-08-13 11:14:18 +0200

    Added db config for prod

--- travelblog/settings.py
+++ travelblog/settings.py
@@ -94,9 +94,11 @@ WSGI_APPLICATION = 'travelblog.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 # @TODO config for production.
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'travelblog': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
+        }
     }
 }
 
