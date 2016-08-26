8c9f0fd98e75c795037934e56f49a0957515406a
Gael Le Bon <gael.m.lebon@gmail.com>
2016-08-26 11:29:50 +0200

    Added template for gallery detail, fixed path in dev_settings.py, changed css for base background & colors, added titles

--- travelblog/dev_settings.py
+++ travelblog/dev_settings.py
@@ -17,3 +17,15 @@ DATABASES = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+#Media files
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
