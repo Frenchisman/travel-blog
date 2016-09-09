06854dce689a3a4bb9a8a8083115c84e32a6b4e8
Gaël Le Bon <gael.m.lebon@gmail.com>
2016-09-09 18:53:36 +0200

    Error pages

--- travelblog/urls.py
+++ travelblog/urls.py
@@ -18,6 +18,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
 from django.conf import settings
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 urlpatterns = [
     # Admin pages
@@ -33,6 +35,31 @@ urlpatterns = [
     url(r'^blog/', include('zinnia.urls')),
     url(r'^comments/', include('django_comments.urls')),
 ]
+
+def handler400(request):
+    response = render_to_response('400.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 400
+    return response
+
+def handler403(request):
+    response = render_to_response('403.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 403
+    return response
+
+def handler404(request):
+    response = render_to_response('404.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
+
+
+def handler500(request):
+    response = render_to_response('500.html', {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 500
+    return response
 
 if settings.DEBUG:
     urlpatterns += static(
